e7cacd7f469ddd80af944a05aad78e5e5cce357f
ZipZap <ghalipm@gmail.com>
2024-07-30 00:32:16 -0400

    create tests

---
+++ src/test/java/com/cydeo/tests/dayx/Equality.java
@@ -0,0 +1,15 @@
+package com.cydeo.tests.dayx;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ghalipm on 7/30/2024
+ * @project playwright-with-java
+ */
+public class Equality {
+    @Test
+    public void test01(){
+        Assertions.assertEquals(5,5);
+    }
+}
